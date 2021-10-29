3c8f7c847dd38dd5332dca26cdf544d5342129ff
PurpleNoodlesQwQ <93408863+PurpleNoodlesQwQ@users.noreply.github.com>
2021-10-29 17:48:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+documento . querySelector ( ".menu-btn" ) . addEventListener ( "clic" ,  ( )  =>  {
+    documento . querySelector ( ".nav-menu" ) . classList . alternar ( "mostrar" ) ;
+  } ) ;
+  
+  ScrollReveal ( ) . revelar ( '.showcase' ) ;
+  ScrollReveal ( ) . revelar ( '.news-cards' ,  {  retraso : 500  } ) ;
+  ScrollReveal ( ) . revelar ( '.cards-banner-one' ,  {  retraso : 500  } ) ;
+  ScrollReveal ( ) . revelar ( '.cards-banner-two' ,  {  retraso : 500  } ) ;
